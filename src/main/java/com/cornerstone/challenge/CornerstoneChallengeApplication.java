73dc2dee79092ee53dfb7dc2347688848f65d20d
julianRosales <rjulianbenjamin@gmail.com>
2024-08-08 17:39:10 -0300

    Creacion

---
+++ src/main/java/com/cornerstone/challenge/CornerstoneChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.cornerstone.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CornerstoneChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CornerstoneChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cornerstone/challenge/CornerstoneChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cornerstone.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CornerstoneChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
